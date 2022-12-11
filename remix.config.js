4a7f17593f9f4f2203a8f32c09294eb2c114382a
monosniper <vanteateam@gmail.com>
2022-12-11 19:43:24 +0500

    auth

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  serverDependenciesToBundle: ["@apollo/client", "axios"],
+  serverDependenciesToBundle: ["@apollo/client", "axios", "zen-observable-ts", "ts-invariant"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
